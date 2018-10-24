8e86a6b9ac5e45daf2013af6bc4fb34c5d7ee012
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-10-24 17:28:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function(e) {
   document.getElementById("FloralButton").addEventListener("click", function(){
     document.getElementById("Rings").style.display = "none";
     document.getElementById("Floral").style.display = "inline-block";
