63968b1a0f9695436b16bff8813828f9b2b10d55
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-10-24 17:33:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,16 @@ document.addEventListener("DOMContentLoaded", function(e) {
     document.getElementById("Floral").style.display = "inline-block";
     document.getElementById("Pink").style.display = "none";
   });
+  
+    document.getElementById("FloralButton").addEventListener("click", function(){
+    document.getElementById("Rings").style.display = "none";
+    document.getElementById("Floral").style.display = "inline-block";
+    document.getElementById("Pink").style.display = "none";
+  });
+  
+    document.getElementById("FloralButton").addEventListener("click", function(){
+    document.getElementById("Rings").style.display = "none";
+    document.getElementById("Floral").style.display = "inline-block";
+    document.getElementById("Pink").style.display = "none";
+  });
 })
