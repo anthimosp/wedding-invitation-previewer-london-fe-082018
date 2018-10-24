fdc8c2ea9045a0434d0a299c87e05137bd8e88d6
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-10-24 18:14:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ document.addEventListener("DOMContentLoaded", function(e) {
   });
   
   document.getElementById("FirstNamePopulate").addEventListener("keypress", function(e){
-    document.getElementById("FirstNameFloat").innerHTML = "FirstNameFloat";
+    document.getElementById("FirstNameFloat").innerHTML = FirstNameFloat;
   });
 })
