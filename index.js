00dbb3ba52a2ea647cbeff3a05775e73d8c0ed44
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-10-24 17:20:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 document.getElementById("button").addEventListener("click", function(){
-    document.getElementById("Floral").innerHTML = Floral;
+    document.getElementById("Floral").innerHTML = "Floral";
 });
